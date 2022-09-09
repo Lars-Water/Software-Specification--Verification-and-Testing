df236413b90dcfcad9f3d4d753d67f8ebb2dbf45
jajayumei <87029269+jajayumei@users.noreply.github.com>
2022-09-09 18:39:29 +0200

    Corrected luhn algorithm

--- Exercise7.hs
+++ Exercise7.hs
@@ -7,7 +7,7 @@ import Data.List
 
 
 luhn :: Integer -> Bool
-luhn x =  (sum (map (\(x,y) -> if y `mod` 2 == 0 then x else if x * 2 > 9 then x * 2 - 9 else x * 2) (zip (map (\x -> read [x] :: Integer) (reverse (show x))) [1..]))) `mod` 10 == 0
+luhn x =  (sum (map (\(x,y) -> if y `mod` 2 == 0 then x else if x * 2 > 9 then x * 2 - 9 else x * 2) (zip (map (\x -> read [x] :: Integer) (reverse (show x))) [0..]))) `mod` 10 == 0
 
 isAmericanExpress, isMaster, isVisa::  Integer -> Bool
 isAmericanExpress x = (length (show x) == 15) && (take 2 (show x) `elem` ["37", "34"]) && (luhn x)
