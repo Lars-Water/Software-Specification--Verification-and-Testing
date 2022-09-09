24ecc27d1af42bb3d13bc285b779989dc37d3800
Lars-Water <larryvanderwater@hotmail.com>
2022-09-09 23:08:58 +0200

    Update Exercise8.hs
    Added description of the process and state of the exercise;

--- Lab1/Exercise8.hs
+++ Lab1/Exercise8.hs
@@ -1,8 +1,26 @@
 import Lab1
 
 {-
-    Determine the accusation of one boy about another boy.
+    Time spent on this exercise: 60 minutes.
+
+    We are not sure how to succesfully solve for this exercise. Furthermore we are uncertain about the implementation
+    of the accuses function. Accusation is ambiguous in what is actually being accused for: stealing or lying.
+
+    The implementation now returns a single Boolean value if an accusation is in the context of stealing (Mattew and Peter),
+    and we are quite confident with this part of the implementation.
+
+    Furthermore, the function calls upon the function recursively when a statement is in the context of lying, as these
+    statements follow down a path to a final statement for stealing.
+    If the accused boy argument is part of the accusers statement, this statement is syntactically formed in a conditional
+    statement.
+
+    Nevertheless, the recursive functions appear to be called upon inaccurately. The only accused boy ever
+    passed on is analoguous to the accuser of the recursive function. We used pattern matching in the function
+    to try and recreate the accuser's statement as they accuse only a specified number of boys, but this pattern
+    matching actually results in the faulty recursive function call.
 -}
+
+
 accuses ::  Boy -> Boy -> Bool
 accuses accuser accused
     | accuser == Matthew && (accused == Carl || accused == Matthew) = False -- Matthew does not accuse Carl and himself.
@@ -12,12 +30,6 @@ accuses accuser accused
     | accuser == Carl && accused == Arnold                          = not $ accuses Arnold accused  -- Arnold is lying.
     | otherwise = False
 
-{-
-    Select the boys that accuse the requested boy. All possible boys are
-    determined for their accusation by use of the accuses function. If this
-    function returns true the boy is appended to the list.
 
-    accused: Boy accused
--}
 accusers ::  Boy -> [Boy]
 accusers accused = [accuser | accuser <- boys, accuses accuser accused]
