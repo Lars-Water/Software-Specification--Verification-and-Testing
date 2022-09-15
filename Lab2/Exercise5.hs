d6cdf2232904be647c7a736c8a57286248d6fa0c
Lars-Water <larryvanderwater@hotmail.com>
2022-09-15 15:02:51 +0200

    Function exercise 5 implemented
    Don't know how to correctly import "isPermutation" function from exercise4. So now copy pasted into this function;

--- Lab2/Exercise5.hs
+++ Lab2/Exercise5.hs
@@ -3,8 +3,14 @@ import Data.Char
 import System.Random
 import Test.QuickCheck
 
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation [] []         = True
+isPermutation [] _          = False
+isPermutation _ []          = False
+isPermutation (x:xs) ys     = x `elem` ys && isPermutation xs (x `delete` ys)
+
 isDerangement :: Eq a => [a] -> [a] -> Bool
-isDerangement [] [] = True
-isDerangement _  [] = False
-isDerangement [] _  = False
-isDerangement (x:xs) (y:ys) = x /= y && x `elem` (y:ys)
+isDerangement xs ys = isPermutation xs ys && all (==True) (zipWith (/=) xs ys)
+
+deran :: Int -> [[Int]]
+deran n = filter (\x -> isDerangement x [1..n]) (permutations [1..n])
