f601fdc6551107a99b925ccef87fced3fea5bc69
Lars-Water <larryvanderwater@hotmail.com>
2022-09-30 10:23:35 +0200

    Set out function;

--- Lab4/Exercise5.hs
+++ Lab4/Exercise5.hs
@@ -1,10 +1,12 @@
 import Data.List
 import LTS
 import Test.QuickCheck
-import Exercise4
 
-out :: IOLTS -> State -> [State]
+out :: IOLTS -> State -> [Label]
 out (_, _, outputLabels, labeledTransitions, _) curState =
-    map thd (filter (\ (state, label, _) -> label `elem` outputLabels && state == curState) labeledTransitions)
+    map second (filter filterOutputLabels labeledTransitions)  --  Return map of output labels from requested state
+        where second             = \(_, label, _) -> label     --  Return second element of a triple tuple
+              filterOutputLabels = \(state, label, _) ->       --  Return TransitionLabels that contain outputlabels and requested state
+                label `elem` outputLabels && state == curState
 
 -- ioco ::
