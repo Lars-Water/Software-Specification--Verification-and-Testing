cb08fac7fd7fc83654007fe4de6d0a529803bc9d
Lars-Water <54835307+Lars-Water@users.noreply.github.com>
2022-10-07 18:00:10 +0200

    Update Exercise5.hs

--- Week 5/Lab/Exercise5.hs
+++ Week 5/Lab/Exercise5.hs
@@ -1,5 +1,5 @@
 -- This file is property of the group Notorious Fortunate Panda © 2022
--- Time spent on this exercise was: 4.5 hours
+-- Time spent on this exercise was: ?? minutes
 
 module Exercise5 where
 import Mutation
@@ -9,25 +9,36 @@ import Exercise3
 import MultiplicationTable
 import Test.QuickCheck
 import Data.List
-import Data.Function
 import Data.Maybe
 
 {-
-    To calculate the conjectures
-    we will first create the powerset of (list of) properties
-    to get all the possible combinations of properties.
-    Then we will count the number of survivors for each combination
-    and determine based on the survivors which conjectures can be made.
 
-    Example:
-    Properties: [1,2,3]
-    Combinations: [[1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
-    Survivors: [1, 2, 0, 2, 0, 0, 0]
-    Some example conjectures: 1 ==> 2, 3 ==> 1, [1,3] == [2,3]
-    Do note that this function will not be able to check for disjunctions.
-    So both property 1 and 2 might have 1 survivor and we will state them as equivalent,
-    but the survivor might be different making them disjoint
+    Conjectures equivalence:
+        The approach to calculating equivalence conjectures starts with defining the powerset of the list of properties. Subsequently,
+        mutation tests are performed on every subset in this powerset. Specification of the mutants that were killed should be returned
+        from these mutation tests. Consequently, equivalence conjectures can be determined through comparison of the killed mutants where
+        an equal return value implies equivalence.
+    Conjecture implication:
+        ...
+
 -}
+
+-- Generate powerset of propery list: https://medium.com/@angerman/powersets-in-haskell-1df9684db52a
+testPower :: [a -> Integer -> Bool] -> [[a -> Integer -> Bool]]
+testPower [] = [[]]
+testPower (x:xs) = map (x:) (testPower xs) ++ testPower xs
+
+survivors mutator n_mutants props func = do
+    let inputs = [0..n_mutants]
+    results <- generate $ mapM (\x -> survived mutator props func x) inputs
+    return results
+
+-- Input:
+--  Survivors per subset
+equivalence :: [([Bool],[Char])] -> [[([Char],Bool)]]
+equivalence survivorsSubsets = do
+    let equivalence_list = map (\(subset,name) -> map (\(compareSubset,compareName) -> (name ++ " == " ++ compareName, compareSubset == subset)) (delete (subset,name) survivorsSubsets)) survivorsSubsets
+    return $ filter (\(_,check) -> check) (concat equivalence_list)
 
 -- Returns the number of survivors for each property subset
 -- Type definition of the function: mutator -> number of mutants -> [(properties, name of property)] -> function -> [Number of survivors]
@@ -37,10 +48,27 @@ surviversPerProps mutator n_mutants props fun = do
     survivors <- sequence $ map (\propSubset -> countSurvivors mutator n_mutants (map (\(prop,name) -> prop) propSubset ) fun) powerProps
     return $ survivors
 
+
 main5 :: IO ()
 main5 = do
-    let properties = [(prop_tenElements, "prop_tenElements"), (prop_linear, "prop_linear"), (prop_firstElementIsInput, "prop_firstElementIsInput")]
+    let properties = [(prop_tenElements, "prop_tenElements"), (prop_firstElementIsInput, "prop_firstElementIsInput")]
     let props = map (\x -> map (\(prop,name) -> name ) x) (powerSet properties)
     print props
     testOne <- generate $ surviversPerProps addElements 3000 properties multiplicationTable
     print $ testOne
+
+    let powersetProps = testPower [prop_tenElements, prop_firstElementIsInput]   --  Determine powerset of props
+    mutantSurvivors <- sequence $ map (\subsetProps ->                           --  Determine mutant survivors per subset
+        survivors addElements 100 subsetProps multiplicationTable) powersetProps
+    let equivalences = equivalence (zip mutantSurvivors ["prop_tenElements & prop_firstElementIsInput", "prop_firstElementIsInput", "prop_tenElements", "NULL"])
+    return equivalences
+
+-- -- main5 :: IO [[Bool]]
+-- main5 = do
+--     let powersetProps = testPower [prop_tenElements, prop_firstElementIsInput]   --  Determine powerset of props
+--     mutantSurvivors <- sequence $ map (\subsetProps ->                           --  Determine mutant survivors per subset
+--         survivors addElements 100 subsetProps multiplicationTable) powersetProps
+--     -- Determine equivalences between the subsets.
+--     let equivalences = equivalence (zip mutantSurvivors ["prop_tenElements & prop_firstElementIsInput", "prop_firstElementIsInput", "prop_tenElements", "NULL"])
+--     return equivalences
+--     -- return mutantSurvivors
