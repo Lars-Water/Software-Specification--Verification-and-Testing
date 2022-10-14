fd82d109bba5764836a922da560c584f65fb0d43
jajayumei <87029269+jajayumei@users.noreply.github.com>
2022-10-14 16:09:04 +0200

    Did someone ask for extra properties

--- Week 6/Exercise2.hs
+++ Week 6/Exercise2.hs
@@ -11,6 +11,11 @@ import Data.List
 getLength :: Ord a => Set a -> Int
 getLength (Set xs) = length xs
 
+-- Helper function to test if a set only contains unique elements
+uniqueElems :: Ord a => Set a -> Bool
+uniqueElems (Set []) = True
+uniqueElems (Set (x:xs)) = not (elem x xs) && uniqueElems (Set xs)q
+
 setIntersection ::   Ord a => Set a -> Set a -> Set a
 setIntersection (Set xs) (Set ys) = Set (intersect xs ys)
 
@@ -27,74 +32,92 @@ setDifference (Set xs) (Set ys) = Set (xs \\ ys)
 -- EEN VOORBEELD KAN ZIJN BIJ INTERSECT: TESTEN OF INTERSECT ALLEEN UNIEKE ELEMENTEN BEVAT,
 -- TESTEN OF INTERSECT ALLEEN ELEMENTEN BEVAT DIE IN BEIDE SETS ZITTEN
 
--- Property to test set intersection + difference -> the intersect of (the intersect of two lists + the difference of two lists) 
--- can be at most the length of 0
-intersectOfDifferenceAndIntersect :: Ord a => Set a -> Set a -> Bool
-intersectOfDifferenceAndIntersect set1 set2 = length == 0
-    where length = getLength $ setIntersection (setIntersection set1 set2) (setDifference set1 set2)
+-- Properties for intersection
 
--- Property to test set difference -> the intersect of (the difference of two lists + the difference of two lists with the lists 
--- placed the other way around) can be at most the length of 0
-intersectOfDifferenceAndDifference :: Ord a => Set a -> Set a -> Bool
-intersectOfDifferenceAndDifference set1 set2 = length == 0
-    where length = getLength $ setIntersection (setDifference set1 set2) (setDifference set2 set1)    
+-- Property to test set intersection -> the intersection of two sets should only contain unique elements
+uniqueElemsIntersect :: Ord a => Set a -> Set a -> Bool
+uniqueElemsIntersect set1 set2 = uniqueElems $ setIntersection set1 set2
 
 -- Property to test set intersection -> the intersect of two lists can be at most the length of the smallest list
 maxIntersectLength :: Ord a => Set a -> Set a -> Bool
 maxIntersectLength set1 set2 =  intersectLength <= getLength set1 && intersectLength <= getLength set2
     where intersectLength = getLength (setIntersection set1 set2)
 
--- Property to test set union -> the union of two lists can be at most the length of the sum of the lengths of the two lists
-maxUnionLength :: Ord a => Set a -> Set a -> Bool
-maxUnionLength set1 set2 = unionLength <= getLength set1 + getLength set2
-    where unionLength = getLength (setUnion set1 set2)
-
--- Property to test set difference -> the difference of two lists can be at most the length of the first list
-maxDifferenceLength :: Ord a => Set a -> Set a -> Bool
-maxDifferenceLength set1 set2 = differenceLength <= getLength set1
-    where differenceLength = getLength (setDifference set1 set2)
-    
--- Property to test if Union of an arbitrary set with the empty set will return the arbitrary set.
-emptySetUnion :: Ord a => Set a -> Bool
-emptySetUnion set1 = (setUnion set1 emptySet) == set1
-
 -- Property to test if Intersection of an arbitrary set with the empty set will return the empty set.
 emptySetIntersection :: Ord a => Set a -> Bool
 emptySetIntersection set1 = isEmpty $ setIntersection set1 emptySet
-
--- Test idempotent property of Set Union. The unification of an arbitrary set with itself returns the arbitrary set.
-idempotenceUnion :: Ord a => Set a -> Bool
-idempotenceUnion set1 = (setUnion set1 set1) == set1
 
 -- Test idempotent property of Set Intersection. The intersection of an arbitrary set with itself returns the arbitrary set.
 idempotenceIntersection :: Ord a => Set a -> Bool
 idempotenceIntersection set1 = (setUnion set1 set1) == set1
 
--- Test commutative property of Set Union. The order of the unification on the sets does not change the returned set.
-commutativityUnion :: Ord a => Set a -> Set a -> Bool
-commutativityUnion set1 set2 = (setUnion set1 set2) == (setUnion set2 set1)
-
 -- Test commutative property of Set Intersection. The order of the intersection on the sets does not change the returned set.
 commutativityIntersection :: Ord a => Set a -> Set a -> Bool
 commutativityIntersection set1 set2 = (setIntersection set1 set2) == (setIntersection set2 set1)
 
--- Test associative property of Set Union. Sets under Union grouped through parentheses return the same set when the parentheses are changed.
-associativityUnion :: Ord a => Set a -> Set a -> Set a -> Bool
-associativityUnion set1 set2 set3 = (setUnion set1 (setUnion set2 set3)) == (setUnion set3 (setUnion set1 set2))
-
 -- Test associative property of Set Intersection. Sets under Intersection grouped through parentheses return the same set when the parentheses are changed.
 associativityIntersection :: Ord a => Set a -> Set a -> Set a -> Bool
 associativityIntersection set1 set2 set3 = (setIntersection set1 (setIntersection set2 set3)) == (setIntersection set3 (setIntersection set1 set2))
+
+-- Test distributive property of Set Intersection. Intersection of a set with grouped sets under Union return the same set as
+-- Union of all individual Intersection of the single set with the grouped sets.
+distributivityIntersection :: Ord a => Set a -> Set a -> Set a -> Bool
+distributivityIntersection set1 set2 set3 = (setIntersection set1 (setUnion set2 set3)) == setUnion (setIntersection set1 set2) (setIntersection set1 set3)
+
+
+-- Properties for union
+
+-- Property to test set union -> the union of two sets should only contain unique elements
+uniqueElemsUnion :: Ord a => Set a -> Set a -> Bool
+uniqueElemsUnion set1 set2 = uniqueElems $ setUnion set1 set2
+
+-- Property to test set union -> the union of two lists can be at most the length of the sum of the lengths of the two lists
+maxUnionLength :: Ord a => Set a -> Set a -> Bool
+maxUnionLength set1 set2 = unionLength <= getLength set1 + getLength set2
+    where unionLength = getLength (setUnion set1 set2)
+
+-- Property to test if Union of an arbitrary set with the empty set will return the arbitrary set.
+emptySetUnion :: Ord a => Set a -> Bool
+emptySetUnion set1 = (setUnion set1 emptySet) == set1
+
+-- Test idempotent property of Set Union. The unification of an arbitrary set with itself returns the arbitrary set.
+idempotenceUnion :: Ord a => Set a -> Bool
+idempotenceUnion set1 = (setUnion set1 set1) == set1
+
+-- Test commutative property of Set Union. The order of the unification on the sets does not change the returned set.
+commutativityUnion :: Ord a => Set a -> Set a -> Bool
+commutativityUnion set1 set2 = (setUnion set1 set2) == (setUnion set2 set1)
+
+-- Test associative property of Set Union. Sets under Union grouped through parentheses return the same set when the parentheses are changed.
+associativityUnion :: Ord a => Set a -> Set a -> Set a -> Bool
+associativityUnion set1 set2 set3 = (setUnion set1 (setUnion set2 set3)) == (setUnion set3 (setUnion set1 set2))
 
 -- Test distributive property of Set Union. Union of a set with grouped sets under Intersection return the same set as
 -- Intersection of all individual Unions of the single set with the grouped sets.
 distributivityUnion :: Ord a => Set a -> Set a -> Set a -> Bool
 distributivityUnion set1 set2 set3 = (setUnion set1 (setIntersection set2 set3)) == setIntersection (setUnion set1 set2) (setUnion set1 set3)
 
--- Test distributive property of Set Intersection. Intersection of a set with grouped sets under Union return the same set as
--- Union of all individual Intersection of the single set with the grouped sets.
-distributivityIntersection :: Ord a => Set a -> Set a -> Set a -> Bool
-distributivityIntersection set1 set2 set3 = (setIntersection set1 (setUnion set2 set3)) == setUnion (setIntersection set1 set2) (setIntersection set1 set3)
+
+-- Properties for Difference
+
+-- Property to test set difference -> the difference of two sets should only contain unique elements
+uniqueElemsDiff :: Ord a => Set a -> Set a -> Bool
+uniqueElemsDiff set1 set2 = uniqueElems $ setDifference set1 set2
+
+-- Property to test set difference -> the difference of two lists can be at most the length of the first list
+maxDifferenceLength :: Ord a => Set a -> Set a -> Bool
+maxDifferenceLength set1 set2 = differenceLength <= getLength set1
+    where differenceLength = getLength (setDifference set1 set2)
+
+-- Property to test set intersection + difference -> the intersect of (the intersect of two lists + the difference of two lists) 
+-- can be at most the length of 0
+intersectOfDifferenceAndIntersect :: Ord a => Set a -> Set a -> Bool
+intersectOfDifferenceAndIntersect set1 set2 = isEmpty $ setIntersection (setIntersection set1 set2) (setDifference set1 set2)
+
+-- Property to test set difference -> the intersect of (the difference of two lists + the difference of two lists with the lists 
+-- placed the other way around) can be at most the length of 0
+intersectOfDifferenceAndDifference :: Ord a => Set a -> Set a -> Bool
+intersectOfDifferenceAndDifference set1 set2 = isEmpty $ setIntersection (setDifference set1 set2) (setDifference set2 set1)
 
 
 main2 :: IO ()
@@ -291,3 +314,28 @@ main2 = do
     print "Applying quickCheck to the property:"
     quickCheck $ forAll quickCheckGenerator $ \set1 -> forAll quickCheckGenerator $ \set2 -> forAll quickCheckGenerator $ \set3 -> associativityIntersection set1 set2 set3
    
+    print "Testing the property that the intersection of two sets only contains unique elements"
+    print "QuickCheck generator:"
+    set1 <- generate quickCheckGenerator
+    set2 <- generate quickCheckGenerator
+    print set1
+    print set2
+    print "Applying quickCheck to the property:"
+    quickCheck $ forAll quickCheckGenerator $ \set1 -> forAll quickCheckGenerator $ \set2 -> uniqueElemsIntersect set1 set2
+    print "Testing the property that the union of two sets only contains unique elements"
+    print "QuickCheck generator:"
+    set1 <- generate quickCheckGenerator
+    set2 <- generate quickCheckGenerator
+    print set1
+    print set2
+    print "Applying quickCheck to the property:"
+    quickCheck $ forAll quickCheckGenerator $ \set1 -> forAll quickCheckGenerator $ \set2 -> uniqueElemsUnion set1 set2
+    print "Testing the property that the difference of two sets only contains unique elements"
+    print "QuickCheck generator:"
+    set1 <- generate quickCheckGenerator
+    set2 <- generate quickCheckGenerator
+    print set1
+    print set2
+    print "Applying quickCheck to the property:"
+    quickCheck $ forAll quickCheckGenerator $ \set1 -> forAll quickCheckGenerator $ \set2 -> uniqueElemsDiff set1 set2
+
